1940fc7ba806bd63efe8e2eb9113370ec6802320
d-rocco <dhrocco.dev@gmail.com>
2024-02-29 15:14:57 -0500

    Assign a name to the socket (bind)

--- webserver.c
+++ webserver.c
@@ -1,6 +1,9 @@
+#include <netinet/in.h>
 #include <errno.h>
 #include <stdio.h>
 #include <sys/socket.h>
+
+#define PORT 8080
 
 int main() {
     int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -8,7 +11,20 @@ int main() {
         perror("webserver (socket)");
         return 1;
     }
-    printf("socket created successfully");
+    printf("socket created successfully\n");
+
+    struct sockaddr_in host_addr;
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = PORT;
+    host_addr.sin_addr.s_addr = INADDR_ANY;
+    int host_addrlen = sizeof(host_addr);
+
+    int binding = bind(tcp_socket, (const struct sockaddr *)&host_addr, host_addrlen);
+    if (binding == -1) {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("Bind was successful\n");
 
     return 0;
 }
